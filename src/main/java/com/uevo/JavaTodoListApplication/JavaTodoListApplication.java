8a01d2c57c33254123db7018d3af73bbe6ea44b1
simonjoseph <simaojose1978@gmail.com>
2025-07-23 11:16:17 +0100

    initial

---
+++ src/main/java/com/uevo/JavaTodoListApplication/JavaTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.uevo.JavaTodoListApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uevo/JavaTodoListApplication/JavaTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevo.JavaTodoListApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
