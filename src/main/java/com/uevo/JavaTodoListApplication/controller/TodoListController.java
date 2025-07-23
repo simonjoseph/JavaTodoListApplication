e7d6875f5203d0d150457729d17b6a76bae21c00
simonjoseph <simaojose1978@gmail.com>
2025-07-23 16:02:56 +0100

    fineshed a Todo controller

--- src/main/java/com/uevo/JavaTodoListApplication/controller/TodoListController.java
+++ src/main/java/com/uevo/JavaTodoListApplication/controller/TodoListController.java
@@ -2,8 +2,10 @@ package com.uevo.JavaTodoListApplication.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +37,22 @@ public class TodoListController {
     public TodoList update(@RequestBody TodoList todoList) {
         return todoListService.updateTodoList(todoList.getId(), todoList);
     }
+
+    @GetMapping("/get/{id}")
+    public TodoList getById(@PathVariable Long id) {
+        return todoListService.getTodoListById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        todoListService.deleteTodoList(id);
+    }
+
+    @GetMapping("/completed")
+    public List<TodoList> getCompleted() {
+        return todoListService.getAllTodoLists().stream()
+                .filter(TodoList::getCompleted)
+                .toList();
+    }
+
 }
